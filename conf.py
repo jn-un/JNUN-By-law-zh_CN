0a31e3bba21b917bdf077f26c032545cdc534788
ik <lk.sun@jiangxue.team>
2022-10-04 10:25:54 +0800

    law_zh-CN submodule

---
+++ conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Nowadays - Unite Nowaday by-law'
+copyright = '2022, JNO Works Group'
+author = 'JNO Works Group'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False     # optional.
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,20 @@
+.. Nowadays - Unite Nowaday by-law documentation master file, created by
+   sphinx-quickstart on Tue Oct  4 09:59:31 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Nowadays - Unite Nowaday by-law's documentation!
+===========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
